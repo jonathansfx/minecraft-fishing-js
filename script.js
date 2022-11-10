10c1b9d043a59cf80a9b24daace852ab2522274f
Jonathan Luo <97814319+zombie0000@users.noreply.github.com>
2022-11-10 14:45:59 -0700

    asdf

---
+++ script.js
@@ -0,0 +1,12 @@
+// Minecraft Fishing Simulator
+
+// Variables to Store HTML Elements
+let steveImgEl = document.getElementById("steve-img");
+let alexImgEl = document.getElementById("alex-img");
+let fishBtnEl = document.getElementById("fish-btn");
+let imgResultEl = document.getElementById("img-result");
+let numCodEl = document.getElementById("num-cod");
+let numSalmonEl = document.getElementById("num-salmon");
+let numTropicalEl = document.getElementById("num-tropical");
+let steveImgEl = document.getElementById("num-puffer");
+let steveImgEl = document.getElementById("steve-img");
