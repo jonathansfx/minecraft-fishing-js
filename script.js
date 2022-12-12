4a83f337d18f91fc3e69634112ba95ab2c2aa44b
jonathansfx <jonathanluo22@gmail.com>
2022-12-12 14:43:44 -0700

    puffer not tracking

--- script.js
+++ script.js
@@ -10,7 +10,62 @@ let numSalmonEl = document.getElementById("num-salmon");
 let numTropicalEl = document.getElementById("num-tropical");
 let numPufferEl = document.getElementById("num-puffer");
 
+// Gloal Variables
+let character = "Steve";
+let numCod = 0;
+let numSalmon = 0;
+let numTropical = 0;
+let numPuffer = 0;
+
 // Event Listeners
 steveImgEl.addEventListener("click", selectSteve);
+alexImgEl.addEventListener("click", selectAlex);
+fishBtnEl.addEventListener("click", fishOnce);
 
-function selectSteve() {}
+// Event Functions
+function selectSteve() {
+  // Update Active Border
+  steveImgEl.classList.add("active");
+  alexImgEl.classList.remove("active");
+
+  // Update character selection Variable
+  character = "Steve";
+}
+
+function selectAlex() {
+  // Update Active Border
+  steveImgEl.classList.remove("active");
+  alexImgEl.classList.add("active");
+
+  // Update character selection Variable
+  character = "Alex";
+}
+
+function fishOnce() {
+  // Test current character
+  if (character === "Steve") {
+    // Use Steve Probability Distribution for Fishing
+    let randNum = Math.random();
+
+    if (randNum < 0.7) {
+      numCod++;
+      imgResultEl.src = "img/Raw-Cod.png";
+      numCodEl.innerHTML = numCod;
+    } else if (randNum < 0.9) {
+      numSalmon++;
+      imgResultEl.src = "img/Raw-Salmon.png";
+      numSalmonEl.innerHTML = numSalmon;
+    } else if (randNum < 0.95) {
+      numTropical++;
+      imgResultEl.src = "img/Tropical-Fish.png";
+      numTropicalEl.innerHTML = numTropical;
+    } else {
+      numPuffer++;
+      imgResultEl.src = "img/Pufferfish.png";
+      numPufferEl.innerHTML = numPuffer;
+    }
+  } else {
+    // Use Alex Probability Distribution for Fishing
+    alert("Fish with Alex");
+  }
+}
